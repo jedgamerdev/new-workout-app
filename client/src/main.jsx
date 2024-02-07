8242494f422637545b8f0a95f91cc91b71b62062
jeddeveloper <opaonjade@gmail.com>
2024-02-07 21:40:54 +0800

    Finished Version

---
+++ client/src/main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { WorkoutsContextProvider } from "./context/WorkoutsContext";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <WorkoutsContextProvider>
+      <App />
+    </WorkoutsContextProvider>
+  </React.StrictMode>
+);
